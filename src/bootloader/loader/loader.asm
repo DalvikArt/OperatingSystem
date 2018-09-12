c4e89a934446f2435a7ac13083a62e752e9021b6
dalvikart <chenckluo@hotmail.com>
2018-09-12 22:06:45 +0800

    add A20 line check

--- src/bootloader/loader/loader.asm
+++ src/bootloader/loader/loader.asm
@@ -1,3 +1,5 @@
+[bits 16]
+
 org 0x10000
 
 jmp     Label_Start
@@ -42,8 +44,7 @@ call    Func_PrintString
 
 call    Func_FastGateA20
 
-mov     ax, 0x0010
-mov     fs, ax
+call    check_a20
 
 ; loop wait
 jmp $
@@ -103,9 +104,13 @@ ret     6h
 ;;; Descryption:      Enable a20
 Func_FastGateA20:
 push    ax
-in      al, 92h
-or      al, 00000010b
-out     92h, al
+push    dx
+
+in      al, 0x92
+or      al, 2
+out     0x92, al
+
+pop     dx
 pop     ax
 ret
 
@@ -129,7 +134,65 @@ mov     eax, cr0
 or      eax, 1
 mov     cr0, eax
 
+ret
 
+; Function: check_a20
+;
+; Purpose: to check the status of the a20 line in a completely self-contained state-preserving way.
+;          The function can be modified as necessary by removing push's at the beginning and their
+;          respective pop's at the end if complete self-containment is not required.
+;
+; Returns: 0 in ax if the a20 line is disabled (memory wraps around)
+;          1 in ax if the a20 line is enabled (memory does not wrap around)
+ 
+check_a20:
+    pushf
+    push ds
+    push es
+    push di
+    push si
+ 
+    cli
+ 
+    xor ax, ax ; ax = 0
+    mov es, ax
+ 
+    not ax ; ax = 0xFFFF
+    mov ds, ax
+ 
+    mov di, 0x0500
+    mov si, 0x0510
+ 
+    mov al, byte [es:di]
+    push ax
+ 
+    mov al, byte [ds:si]
+    push ax
+ 
+    mov byte [es:di], 0x00
+    mov byte [ds:si], 0xFF
+
+    cmp byte [es:di], 0xFF
+ 
+    pop ax
+    mov byte [ds:si], al
+ 
+    pop ax
+    mov byte [es:di], al
+ 
+    mov ax, 0
+    je check_a20__exit
+ 
+    mov ax, 1
+ 
+check_a20__exit:
+    pop si
+    pop di
+    pop es
+    pop ds
+    popf
+ 
+    ret
 
 
 ; message string
